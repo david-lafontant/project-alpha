c8af8b65042fc3dcd3f63799120d61b3b8b20261
david-lafontant <lafontant.david@gmail.com>
2023-02-21 11:54:39 -0600

    Clean index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const title = document.createElement("h1");
-title.innerHTML = "Hello World!";
-document.querySelector("body").appendChild(title);
+// const title = document.createElement("h1");
+// title.innerHTML = "Hello World!";
+// document.querySelector("body").appendChild(title);
